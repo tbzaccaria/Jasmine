6474920975c2f63d66b2905fe6e698a64ac7d10f
Tabi Zaccaria <zaccariatabi@gmail.com>
2021-10-12 11:00:32 +0200

    test 3

--- 3.2.Test2/questions.js
+++ 3.2.Test2/questions.js
@@ -1,43 +1,117 @@
 let CreationTableauLangages =  () => {
+    let langages=["Html", "CSS", "Java", "PHP"];
+    return langages;
 
 }
 
-let CreationTableauNombres =  () => {
+let CreationTableauNombres =  (langages=[0, 1, 2, 3, 4, 5]) => {
+    return langages;
 
 }
 
 let RemplacementElement =  (langages) => {
+    let array=[];
+    for (let elem of langages){
+        if (elem ==="Java"){
+            array.push("Javascript");
+
+        }
+        else{
+            array.push(elem);
+        }
+    }
+    return array;
+    
 
 }
 
 let AjoutElementLangages =  (langages) => {
+    langages.push("Ruby");
+    langages.push("Python");
+    return langages;
 
 }
 
 let AjoutElementNombres =  (nombres) => {
+    let newArray=[-2, -1];
+    for (let elem of nombres){
+        newArray.push(elem);
+    }
+    
+    return newArray;
 
 }
 
 let SuppressionPremierElement =  (langages) => {
+    let elem = langages[0];
+    let newArray=[];
+    for(let i=0;i<langages.length;i++){
+        if(langages[i]===elem){
+            
+    
+
+        }
+        else{
+            newArray.push(langages[i]);
+        }
+    }
+    
+
+    
+    
+    return newArray;
 
 }
 
 let SuppressionDernierElement =  (langages) => {
+    let elem = langages[langages.length-1];
+    let newArray=[];
+    for(let i=0;i<langages.length;i++){
+        if(langages[i]===elem){
+            
+    
+
+        }
+        else{
+            newArray.push(langages[i]);
+        }
+    }
+    
+
+    
+    
+    return newArray;
 
 }
 
 let ConversionChaineTableau =  (reseaux_sociaux_chaine) => {
+    let reseaux_sociaux=reseaux_sociaux_chaine.split(",");
+    return reseaux_sociaux;
 
 }
 
 let ConversionTableauChaine =  (langages) => {
+    let str="";
+    for (let i = 0; i < langages.length; i++){
+        if(langages[i]===langages[langages.length-1]){
+            str=str+langages[i];
+        }
+        else{
+            str=str+langages[i]+","
+        }
+    }
+    return str;
 
 }
 
 let TriTableau =  (reseaux_sociaux) => {
+    let reseau =reseaux_sociaux.sort();
+    return reseau;
 
 }
 
 let InversionTableau =  (langages) =>{
+    return langages.reverse();
+
 
 }
--- 3.3.Recall/questions.js
+++ 3.3.Recall/questions.js
@@ -1,33 +1,133 @@
 let selectElementsStartingWithA = (array) => {
-    return 'Write your method here';
+    // init new list
+    let sortA=[];
+    // loop 
+    for (let elem of array){
+        // select A in first character
+        if (elem[0]==="a"){
+            sortA.push(elem);
+        }
+    }
+    return sortA ;
 }
 
 let selectElementsStartingWithVowel = (array) => {
-    return 'Write your method here';
+    // init new array
+    let sortVowel=[];
+    let allVowel=["a", "e", "i", "o", "u", "y"];
+
+    // array loop to check all element of array
+    for (let elem of array){
+
+        // second loop to check if first lette is a vowel
+        
+        for (let i=0; i < allVowel.length; i++ ){
+            if (elem[0] === allVowel[i]){
+
+                // if true push to the sort array
+                sortVowel.push(elem);
+            }
+        }
+    }
+    
+    return sortVowel;
 }
 
 let removeNullElements = (array) => {
-    return 'Write your method here';
+    // init
+    let sortArray=[];
+    // array loop
+    for (let elem of array){
+        // Select other than null
+        if (elem != null){
+            sortArray.push(elem);
+        }
+    }
+
+
+    return sortArray;
 }
 
 let removeNullAndFalseElements = (array) => {
-    return 'Write your method here';
+    // init
+    let sortArray=[];
+    // array loop
+    for (let elem of array){
+        if (elem != null){
+            if (elem === false){
+                console.log(false);
+            }
+            else {
+                sortArray.push(elem);
+            }
+            
+        }
+    }
+
+
+    return sortArray;
 }
 
 let reverseWordsInArray = (array) => {
-    return 'Write your method here';
+    // function reverse Word
+    function reverseWord(word){
+        // split str to a list
+        let wordSplit=word.split('')
+        // reverse element list
+        let array=wordSplit.reverse();
+        // remake a str
+        let wordJoin=array.join();
+        // remove all virgules
+        let result=wordJoin.replace(/,/g,"");
+
+        return result
+        
+    }
+    //init
+    let reverseWords=[];
+
+    // array loop
+    for (let elem of array){
+        let newWord=reverseWord(elem);
+        reverseWords.push(newWord);
+
+    }
+    return reverseWords;
 }
 
 let everyPossiblePair = (array) => {
-    return 'Write your method here';
+    sortInPair=[]
+    for (let elem of array){
+        for (let pair of array){
+            if ((elem != pair) && (!(sortInPair.includes([pair,elem])))){
+                let repetition=sortInPair.includes([pair,elem]);
+                if (repetition===false){
+                    sortInPair.push([elem,pair]);
+                }
+                
+            }
+        }
+        
+    }
+    
+    return sortInPair;
 }
 
 let allElementsExceptFirstThree = (array) => {
-    return 'Write your method here';
+    // init sort array
+    sortArray=[];
+    // loop if index > 2 push to sortArray
+    for (let i = 0; i < array.length; i++){
+        if (i > 2){
+            sortArray.push(array[i]);
+        }
+    }
+    return sortArray;
 }
 
 let addElementToBeginning = (array, element) => {
-    return 'Write your method here';
+    array.unshift(element);
+    return array;
 }
 
 let sortByLastLetter = (array) => {
